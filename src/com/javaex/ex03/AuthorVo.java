20f8146a4fab15cc833e5d683c5a24566bc737fb
Danny <ctct25@naver.com>
2021-07-02 17:54:33 +0900

    21/07/02 JDBC Dao 메소드 파라미터 Vo사용하기

---
+++ src/com/javaex/ex03/AuthorVo.java
@@ -0,0 +1,69 @@
+package com.javaex.ex03;
+
+public class AuthorVo {
+	
+	//이 같은 ValueObject는 90퍼센트는 이렇게 만들어진다.
+	
+	
+	//필드
+	private int authorId;
+	private String authorName;
+	private String authorDesc;
+	
+	
+	//생성자
+	public AuthorVo() {
+		super();
+	}
+	
+	
+	public AuthorVo(String authorName, String authorDesc) {
+		super();
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+
+
+	public AuthorVo(int authorId, String authorName, String authorDesc) {
+		super();
+		this.authorId = authorId;
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+
+	
+	//메소드(게터세터)
+	
+	public int getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getAuthorDesc() {
+		return authorDesc;
+	}
+
+	public void setAuthorDesc(String authorDesc) {
+		this.authorDesc = authorDesc;
+	}
+
+
+	//메소드(일반)
+	@Override
+	public String toString() {
+		return "AuthorVo [authorId=" + authorId + ", authorName=" + authorName + ", authorDesc=" + authorDesc + "]";
+	}
+	
+	
+}
