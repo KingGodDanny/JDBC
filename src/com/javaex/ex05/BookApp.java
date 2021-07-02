a1ae6621fe1de3b107273068229b9623c7253865
Danny <ctct25@naver.com>
2021-07-02 17:57:04 +0900

    21/07/02 BookDao 연습문제

---
+++ src/com/javaex/ex05/BookApp.java
@@ -0,0 +1,43 @@
+package com.javaex.ex05;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+
+		
+		//문제(1번)
+		//작가테이블 책테이블 완성
+		//작가테이블 시퀀스 , 책테이블 시퀀스 완성
+		
+		//authorDao.authorInsert(); 작가데이터 6명 추가
+				
+		//bookDao.bookInsert(); 책8개 추가
+		
+		//책수정 , 삭제 각자알아서 테스트
+		
+		
+		//책리스트 출력
+		//bookDao.getBookList();    -- 8개
+		//////////////////////////////////////////////////////여기까지 1번
+		
+		
+		
+		//문제(2번)
+		//스캐너를 통해서 사용자한테 키워드 입력받음
+		//"검색어를 입력해주세요"
+		//"검색어: 문
+		
+		//bookDao.getBookList(keyword);  문이 들어가는 정보 4개
+		//검색된 책정보
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
